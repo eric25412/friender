958cb508da5c6339b182652b80f48cd28c8803d5
eric lopez <ericlz.25412@gmail.com>
2019-03-09 15:06:21 -0800

    new

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendList = [
+    {
+      name: 'Eric lopez',
+      scores: [
+        "4",
+        "5",
+        "1",
+        "2",
+        "4",
+        "5",
+        "1",
+        "3",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
